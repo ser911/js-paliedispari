fd1063c88dd30bab2499228e05e7ee7c347ff90c
Sergio <sergio.marretta@libero.it>
2021-03-08 16:49:54 +0100

    Pari e dispari

--- js/myscript.js
+++ js/myscript.js
@@ -3,6 +3,7 @@ var evenOdd = prompt("Digita un'opzione: pari o dispari?");
 if ( evenOdd == "pari" || evenOdd == "dispari") {
 
   var userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
   var oneToFiveNumber = Math.floor(Math.random() * 5) + 1;
   console.log(oneToFiveNumber);
   var totalUserNumber = userNumber + oneToFiveNumber;
@@ -13,7 +14,26 @@ function cpuRandom (min, max) {
 }
 
 var random = cpuRandom(1,5)
-console.log(random);
+console.log("Il punteggio della cpu é " + random);
+var totalScore = totalUserNumber + random;
+
+if ( totalScore % 2 == 0) {
+  console.log( totalUserNumber + random + " = Pari!");
+}
+else{
+  console.log(totalUserNumber + random + " = Dispari!");
+}
+
+if (evenOdd == "pari" && totalScore % 2 == 0 ){
+alert("Hai vinto!");
+
+}
+  else if (evenOdd == "dispari" && totalScore % 2 == 1 ) {
+    console.log("Hai vinto!");
+  }
+  else{
+    console.log("Hai perso!");
+  }
 
 
 }
