c4add775913349ffeea6616550fa69f2310d8b72
Sergio <sergio.marretta@libero.it>
2021-03-09 09:28:22 +0100

    Palindrome

--- js/myscript.js
+++ js/myscript.js
@@ -1,23 +1,25 @@
+// PROMPT PER LA SCELTA DEL NUMERO DELL'USER
 var evenOdd = prompt("Digita un'opzione: pari o dispari?");
-
+// ANALISI DEL NUMERO SELEZIONATO: PARI O DISPARI
 if ( evenOdd == "pari" || evenOdd == "dispari") {
 
   var userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
-
+// CREAZIONE NUMERO RANDOM
   var oneToFiveNumber = Math.floor(Math.random() * 5) + 1;
   console.log(oneToFiveNumber);
+  // SOMMA DI NUMERO SELEZIONATO + NUMERO RANDOM
   var totalUserNumber = userNumber + oneToFiveNumber;
   console.log(" Il tuo punteggio é " + totalUserNumber);
-
+// FUNZIONE NUMERO RANDOM PER LA CPU
 function cpuRandom (min, max) {
   return Math.floor(Math.random() * max) + min;
 }
 
-var random = cpuRandom(1,5)
+var random = cpuRandom(1,10)
 console.log("Il punteggio della cpu é " + random);
 var totalScore = totalUserNumber + random;
 
-
+// FUNZIONE CHE ANALIZZA LE POSSIBILI COMBINAZIONI DI DUE NUMERI NELL'AMBITO PARI/DISPARI
 function evenOddgame(num1, num2, sum) {
 
   if ( sum % 2 == 0) {
@@ -31,9 +33,9 @@ function evenOddgame(num1, num2, sum) {
 }
 
 var evenOddGameresult = evenOddgame(totalUserNumber, random, totalScore);
-
+// RISULTATO FINALE
 if (evenOdd == "pari" && totalScore % 2 == 0 ){
-alert("Hai vinto!");
+console.log("Hai vinto!");
 
 }
   else if (evenOdd == "dispari" && totalScore % 2 == 1 ) {
@@ -47,4 +49,23 @@ alert("Hai vinto!");
 }
 else{
   alert("Per procedere dovrai digitare una delle due opzioni: pari o dispari");
+}
+
+var userWord = prompt("Inserisci una parola");
+function paliWord (parola){
+  var wordReverse = "";
+  for (var i = userWord.length -1; i >= 0 ; i--) {
+    wordReverse += userWord[i];
+  }
+  return wordReverse;
+
+}
+var wordRebuilt = paliWord(userWord);
+console.log(userWord, wordRebuilt);
+
+if (wordRebuilt == userWord) {
+  console.log("La parola da te digitata è un palindromo");
+}
+else{
+  console.log("La parola da te digitata non è un palindromo");
 }
